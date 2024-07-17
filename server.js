9bb63ce97864409deb6a1f9964beadf741c9a111
NeedlessCat <kumar29.aayush@gmail.com>
2024-07-18 00:03:27 +0530

    Server upto date

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://prasad-hub-client.vercel.app"],
+    // origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   })
